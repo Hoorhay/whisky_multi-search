8603a0a6b11bb04643766a400adadc827666957c
Juraj Hura <juri.hura@gmail.com>
2026-08-31 15:01:33 +0200

    Change order of urls

--- search_for_whisky.py
+++ search_for_whisky.py
@@ -21,11 +21,11 @@ def main():
 
     urls = [
         f"https://www.svetnapojov.sk/vyhladavanie?search={encoded_query}",
+        f"https://www.alkohol.cz/produkty/uigedail/vyhledavani/?q={encoded_query}",
         f"https://www.deinwhisky.de/search?sSearch={encoded_query}",
         f"https://shop.whiskybase.com/us/search/{encoded_query}/",
         f"https://www.whiskysite.nl/en/search/{encoded_query}/",
         f"https://www.passionforwhisky.com/en/search?controller=search&s={encoded_query}",
-        f"https://www.alkohol.cz/produkty/uigedail/vyhledavani/?q={encoded_query}",
         f"https://www.dramtime.eu/?s={encoded_query}&post_type=product&dgwt_wcas=1&lang=en",
     ]
 
